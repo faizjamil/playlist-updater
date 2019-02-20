e91d7fd4ac65b9003d6ed7850651ddb357d22357
Faizan <faizan34@gmail.com>
2019-02-20 18:14:40 -0500

    use os.walk instead of glob to parse directorie(s)

--- file_parser.py
+++ file_parser.py
@@ -1,11 +1,25 @@
 #use glob to parse directory by file type
 import os
-import glob
+        
 def parse_dir(path) :
-    files = []
-    for file in glob.glob(path +"\\*.html"):
-        print(os.path.basename(file))
-    
+    music_files = []
+    sub_directories =[]
+    #gets root (path of directory passed)
+    for root, dirs, files in os.walk(path):
+        #takes filenames and puts them into list if they are .mp3s or .m4a
+        #files are prefixed by their absolute path
+        #creates list of sub-directories
+        for directory in dirs:
+            if (directory != ''):
+                sub_directories.append(directory)
+        for name in files:
+            if name.endswith('.mp3') or name.endswith('.m4a'):
+                #adds path to beginning of file before appending
+                music_files.append(os.path.join(root, name))
+    for example in music_files:
+        print(example)
+    for x in sub_directories:
+        print(x)
+    return music_files, sub_directories
 
-parse_dir("C:\\Users\\Faizan")
-
+parse_dir("C:\\Users\\Faizan\\Music\\Soundtrack\\Other")
